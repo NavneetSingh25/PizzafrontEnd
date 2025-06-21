bcc17880c8a82b2e3bb2f1bf7f82cb5217f6ca9a
Navneet Singh <navneetsingh9234@gmail.com>
2025-06-21 15:11:01 +0530

    added presenter container pattern

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Login from "./pages/Auth/Login";
+import NotFound from "./pages/Auth/NotFound";
 import Signup from "./pages/Auth/SignUp";
 import Home from "./pages/Home"
 import { Route,Routes } from "react-router-dom";
@@ -7,6 +9,9 @@ function App() {
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/auth/signup" element={<Signup/>} />
+            <Route path="/auth/login" element={<Login/>} />
+
+            <Route path="*" element={<NotFound/>} />
 
         </Routes>
     );
--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,45 +1,58 @@
-import signup from '../../assets/Images/image.png';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import SignUpPresentation from './SignUpPresentation';
+
 function Signup(){
+
+    const [SignUpState,setSignUpState]=useState({
+        firstName:'',
+        email:'',
+        mobileNumber:'',
+        password:''
+    });
+
+    function handleUserInput(e){
+        const {name,value}=e.target;
+        setSignUpState({
+            ...SignUpState,
+            [name]:value
+        })
+    }
+
+    function handleFormSubmit(e){
+        e.preventDefault();
+        console.log(SignUpState)
+
+        if(!SignUpState.firstName || !SignUpState.email || !SignUpState.mobileNumber || !SignUpState.password){
+            toast.error('Missing values from the form');
+            return;
+        }
+
+        if(SignUpState.firstName.length<5 || SignUpState.firstName.length>20){
+            toast.error("First Name should be atleast 5 characters long and maximum 20 characters")
+            return;
+        }
+
+        if(!SignUpState.email.includes('@') || !SignUpState.email.includes('.')){
+            toast.error("Invalid Email address")
+            return;
+        }
+
+        if(SignUpState.mobileNumber.length<10 || SignUpState.firstName.length>12){
+            toast.error("Mobile Number should be between 10-12 characters")
+            return;
+        }
+
+
+    }
+
     return(
-        <>
-            <section className="text-gray-600 body-font">
-                <div className="flex flex-wrap items-center h-screen px-10 py-6 mx-auto">
-                    <div className="hidden pr-0 lg:w-3/5 md:w-1/2 md:pr-16  lg:pr-0 md:block ml-10">
-                    <img src={signup} className='w-78 h-80 object-cover mx-5 ' alt="SignUp" />
-                    </div>
-                    <form className="flex flex-col w-full p-8 mt-10 bg-gray-100 lg:w-2/6 rounded-lg md:w-1/2 md:ml-auto md:mt-0 ">
-                        <h2 className="mb-4 text-lg font-medium text-gray-900 title-font">Sign Up</h2>
-                        <div className="relative mb-4">
-                            <label htmlFor="firstname" className="text-sm text-gray-600 leading-7">First Name</label>
-                        <input type="text" id="firstname" name="first name" required minLength={5} placeholder='John' className="w-full px-3 py-1 text-base mt-2 leading-8 text-gray-700  transition-colors duration-200 ease-in-out border border-gray-300 rounded outline-none  focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 focus:outline-none" />
-                        </div>
-                        <div className="relative mb-4">
-                            <label htmlFor="firstname" className="text-sm text-gray-600 leading-7">Email <span className='text-red-500'>*</span></label>
-                        <input type="email" id="email" name="email" required  placeholder='John@example.com' className="w-full px-3 py-1 text-base mt-2 leading-8 text-gray-700  transition-colors duration-200 ease-in-out border border-gray-300 rounded outline-none  focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 focus:outline-none" />
-                        </div>
+        <SignUpPresentation
+            handleFormSubmit={handleFormSubmit}
+            handleUserInput={handleUserInput}
+        />
+    );
 
-                        <div className="relative mb-4">
-                            <label htmlFor="mobile number" className="text-sm text-gray-600 leading-7">Mobile Number<span className='text-red-500'>*</span></label>
-                        <input type="tel" id="mobile number" name="mobile number" required maxLength={12} placeholder='Enter 10 digit mobile number' className="w-full px-3 py-1 text-base mt-2 leading-8 text-gray-700  transition-colors duration-200 ease-in-out border border-gray-300 rounded outline-none  focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 focus:outline-none" />
-                        </div>
-
-                        <div className="relative mb-4">
-                            <label htmlFor="passsword" className="text-sm text-gray-600 leading-7">Password<span className='text-red-500'>*</span></label>
-                        <input type="password" id="password" name="password" required  placeholder='Enter your password' className="w-full px-3 py-1 text-base mt-2 leading-8 text-gray-700  transition-colors duration-200 ease-in-out border border-gray-300 rounded outline-none  focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 focus:outline-none" />
-                        </div>
-
-                        <button className='w-full px-8 text-lg py-2 border-0 rounded focus:outline-none   text-white font-bold bg-yellow-500  hover:bg-yellow-600'>Create Account</button>
-
-                        <p className='t-3 text-xs text-gray-500 '>Already have an account?
-                            <a href="/login" className='text-yellow-500 '>Login</a>
-                        </p>
-
-
-                    </form>
-                </div>
-            </section>
-        </>
-    )
 }
 
 export default Signup;
